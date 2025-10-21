7d7c16cc2806b448f70e268c19a688d37f8d92b8
Michael Mayer <mayermichael79@gmail.com>
2025-10-21 18:11:12 +0200

    Export "Explanation"

--- src/lightshap/__init__.py
+++ src/lightshap/__init__.py
@@ -2,5 +2,6 @@
 
 from ._version import __version__
 from .explainers import explain_any, explain_tree
+from .explanation import Explanation
 
-__all__ = ["__version__", "explain_any", "explain_tree"]
+__all__ = ["__version__", "explain_any", "explain_tree", "Explanation"]
